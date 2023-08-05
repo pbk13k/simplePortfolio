3d789a40fa3f0b3e17dcbd064cb541b26576c4fb
박병국 <pbk13k@gmail.com>
2023-08-05 13:18:31 +0900

    NFC read paddingintent이용한 읽기 방법을 intent-filter를 사용

--- app/src/main/java/co/kr/nawa/simpleportfolio/menu/view/NfcActivity.kt
+++ app/src/main/java/co/kr/nawa/simpleportfolio/menu/view/NfcActivity.kt
@@ -1,10 +1,16 @@
 package co.kr.nawa.simpleportfolio.menu.view
 
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.NdefMessage
+import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
-import android.widget.Toast
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import co.kr.nawa.simpleportfolio.R
 import co.kr.nawa.simpleportfolio.databinding.ActivityNfcBinding
 import co.kr.nawa.simpleportfolio.menu.viewModel.NfcViewModel
@@ -13,6 +19,8 @@ import co.kr.nawa.simpleportfolio.util.common.logD
 import co.kr.nawa.simpleportfolio.util.dialog.DialogCustom
 import co.kr.nawa.simpleportfolio.util.dialog.DialogItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.Arrays
+
 
 class NfcActivity : ActivityBase<ActivityNfcBinding, NfcViewModel>() {
     override val layoutResourceId: Int
@@ -20,32 +28,26 @@ class NfcActivity : ActivityBase<ActivityNfcBinding, NfcViewModel>() {
     override val viewModel: NfcViewModel by viewModel()
     private var nfcAdapter: NfcAdapter? = null
 
+
     override fun init() {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         if (nfcAdapter != null) {
             //NFC off or NFC card Mode
-            if(!nfcAdapter!!.isEnabled){
-                logD("nfc_off")
-                DialogCustom(DialogItem("NFC 설정","NFC를 기본모드로 설정 해주세요!")){
-                    logD("설정 gogo")
-                }.show(supportFragmentManager,"dialog")
-            }
+            nfcOn()
         }else{
             //기기에서 NFC 있는지 확인
             finish()
             return
-
         }
+
     }
 
     override fun init_Listener() {
-        binding.readBtn.setOnClickListener {
 
-        }
 
         binding.writeBtn.setOnClickListener {
-
+            logD("aa")
         }
     }
 
@@ -56,23 +58,70 @@ class NfcActivity : ActivityBase<ActivityNfcBinding, NfcViewModel>() {
         }
     }
 
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
+    private fun byteArrayToHexaString(bytes: ByteArray): String? {
+        val builder = StringBuilder()
+        for (data in bytes) {
+            builder.append(String.format("%02X ", data))
+        }
+        return builder.toString()
+    }
 
+    override fun onNewIntent(intent: Intent) {
+        //setIntent(intent)
+        logD("onNewIntent")
         logD("action=${intent.action}")
 
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                // Process the messages array.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES,NfcAdapter::class.java)
+        }else{
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+        }?.let { it ->
+            val messages: List<NdefMessage> = it.map { it as NdefMessage }
 
+
+            for (msg in messages){
+
+                for (rec in msg.records){
+
+                    if (rec.tnf == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(
+                            rec.type, NdefRecord.RTD_TEXT)
+                    ) {
+                        val offset=rec.payload[0].toInt() and 63
+                        logD("msg=${String(rec.payload)}")
+                        logD("msg_byteArrayToHexaString=${byteArrayToHexaString(rec.payload)}")
+                        val str=String(rec.payload,offset+1,rec.payload.size -offset - 1,Charsets.UTF_8)
+                        logD("msg11=${str}")
+                        logD("bytes[0].toInt() and 63=${offset}")
+                        logD("rec.payload.siz=${rec.payload.size}")
+                        logD("rec.tnf=${rec.tnf}")
+                        logD("rec.type=${rec.type}")
+                        logD("rec.id=${rec.id}")
+
+                        viewModel.setStr(str)
+                    }
+
+                }
             }
         }
+
+        super.onNewIntent(intent)
     }
 
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_nfc)
-//    }
+
+    //nfc 무조건 활성화 시키기
+    private fun nfcOn(){
+        if(!nfcAdapter!!.isEnabled){
+            DialogCustom(DialogItem("NFC 설정","NFC를 기본모드로 설정 해주세요!")){
+                activityLauncher.launch(Intent(Settings.ACTION_NFC_SETTINGS))
+            }.show(supportFragmentManager,"dialog")
+        }
+
+    }
+
+    private val activityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            _: ActivityResult ->
+        //시스템 설정화면에 갔다가 왔을때는  RESULT_CANCELED 옴
+        nfcOn()
+    }
 }
--- app/src/main/java/co/kr/nawa/simpleportfolio/menu/viewModel/NfcViewModel.kt
+++ app/src/main/java/co/kr/nawa/simpleportfolio/menu/viewModel/NfcViewModel.kt
@@ -6,6 +6,13 @@ import co.kr.nawa.simpleportfolio.util.basic.ViewModelBasic
 
 
 class NfcViewModel: ViewModelBasic() {
-    var _str= MutableLiveData<String>()
+
+
+    var _str= MutableLiveData<String>("NFC를 가져다 되면 읽습니다.")
     val str: LiveData<String> get() = _str
+
+
+    fun setStr(s: String) {
+        _str.postValue(s)
+    }
 }
